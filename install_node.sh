2375c744c763d2a7f912fc961aa42eb1492978a2
koooge <koooooge@gmail.com>
2018-02-14 13:36:19 +0900

    update node 8.x

--- install_node.sh
+++ install_node.sh
@@ -4,8 +4,8 @@
 
 shopt -s expand_aliases
 
-NVM_VERSION="v0.33.2"
-NODE_VERSION="lts/boron"
+NVM_VERSION="v0.33.8"
+NODE_VERSION="lts/carbon"
 
 install_nvm() {
   curl -sS -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
@@ -16,10 +16,12 @@ install_nvm() {
 install_node() {
   nvm install ${NODE_VERSION}
   nvm use ${NODE_VERSION}
+  nvm alias default ${NODE_VERSION}
 }
 
 install_npm_global() {
-  npm install -g aws-sdk
+#  npm install -g aws-sdk
+  npm install -g express-generator
 }
 
 install_nvm
